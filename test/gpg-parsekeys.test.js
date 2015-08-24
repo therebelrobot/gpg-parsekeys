59ad1ea6fbc9cf2495fd7cbaf502e0d3154b04bf
Trent Oswald <trentoswald@therebelrobot.com>
2015-08-24 16:57:24 -0700

    [#2] More work on testing

--- test/gpg-parsekeys.test.js
+++ test/gpg-parsekeys.test.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect
 var parse = require('../gpg-parsekeys.js')
 var fs = require('fs')
+var cliParsed = require('./fixtures/cli-parsed.json')
+var curlParsed = require('./fixtures/curl-parsed.json')
 
 /* Definitions for JS Standard */
 /* global describe, it */
@@ -10,23 +12,23 @@ describe('gpg-parsekeys', function () {
     expect(parse).to.be.an('function')
     done()
   })
-  it('should parse curl data into coherent key objects', function (done) {
-    fs.readFile(__dirname + '/fixtures/curl-output.txt', function (err, data) {
-      if (err) throw err
-      console.log(data.toString())
-      data = data.toString()
-      var parsedData = parse(data)
-      console.log(parsedData)
-      done()
-    })
-  })
   it('should parse cli data into coherent key objects', function (done) {
     fs.readFile(__dirname + '/fixtures/cli-output.txt', function (err, data) {
       if (err) throw err
-      console.log(data.toString())
       data = data.toString()
       var parsedData = parse(data)
-      console.log(parsedData)
+      expect(parsedData).to.be.an('array')
+      expect(parsedData).to.deep.equal(cliParsed)
+      done()
+    })
+  })
+  it('should parse curl data into coherent key objects', function (done) {
+    fs.readFile(__dirname + '/fixtures/curl-output.html', function (err, data) {
+      if (err) throw err
+      data = data.toString()
+      var parsedData = parse(data)
+      expect(parsedData).to.be.an('array')
+      expect(parsedData).to.deep.equal(curlParsed)
       done()
     })
   })
